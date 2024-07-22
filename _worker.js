ef1d0349903564bfc55bc48a92bd51354d73ba27
SHE Noah <1170359496@qq.com>
2024-07-22 23:34:52 +0800

    固定IP

--- _worker.js
+++ _worker.js
@@ -17,7 +17,7 @@ const defaultHttpPorts = ['80', '8080', '2052', '2082', '2086', '2095', '8880'];
 const defaultHttpsPorts = ['443', '8443', '2053', '2083', '2087', '2096'];
 
 // let proxyIP = proxyIPs[Math.floor(Math.random() * proxyIPs.length)];
-let proxyIP = '202.144.194.172';
+let proxyIP = '8.219.240.160';
 
 let dohURL = 'https://cloudflare-dns.com/dns-query';
 
